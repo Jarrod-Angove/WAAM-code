a72e527c757450eb7a8c365b3b21db6924e22d2c
Jarrod Angove <jarrodangove@Jarrods-MacBook-Air.local>
2022-12-12 08:34:26 -0800

    added more documentation

---
+++ TODO.md
@@ -0,0 +1,17 @@
+---
+title: "To do list for WAAM code repo"
+author: "Jarrod Angove"
+---
+
+* Break up scripts into functions
+* Turn all of the files into a single julia module
+* Make all of the functions act on directories, so that it can be used on other computers
+- Finish the makie tool
+    - Add temperature range selection via text box
+    - Add time range selection via text input
+    - Function to add multiple cooling rate slopes and image export
+    - Add labels to the best fit curves
+    - Calculate $R^2$ for the fit lines
+    - Add the ability to push to a file? 
+- Future: use GPT to convert everything to python scripts
+
--- thermo_clean.jl
+++ thermo_clean.jl
@@ -1,6 +1,6 @@
 # Script for cleaning up the thermocouple files and creating the heatmaps 
 
-using DataFrames, CSV, PlotlyJS, PrettyTables, Unitful 
+using DataFrames, CSV, Plots, PrettyTables, Unitful 
 
 frames = []
 files = readdir("./thermo_data/")
@@ -46,55 +46,28 @@ for file in files
         eval(Meta.parse("updated[:, :T$i] = tmat[:, $i]"))
     end
 
-    CSV.write("./thermo_cleaned_data/$(nm)_thermotemps.csv", updated)
+    # CSV.write("./thermo_cleaned_data/$(nm)_thermotemps.csv", updated)
         
     # function that takes the vector and reshapes it into the 4x4 array
     function to_heat(x::Vector{Float64})
         hcat(x[[3, 7 , 11, 15]], x[[2, 6, 10, 14]], x[[1, 5, 9, 13]])'
     end
-
+    
+    # Making the time start from 0 and formatting the number
     transform!(df, "temps" => (x -> to_heat.(x)) => "heatmap")
     df.time = ((df.seconds .- df.seconds[1]).*1u"s"
     .+ (df.nanosecs .- df.nanosecs[1]).*1u"ns") .|> Float64
-
     
-xdat = 1:4
-ydat = 1:3
-zdat = ustrip.(df.time)
-values = reshape(mapreduce(permutedims, vcat, df.heatmap), (3, 4, lastindex(df.heatmap)))
-X, Y, Z = mgrid(xdat, ydat, zdat)
+    #Creating the animation
+    anim = @animate for i in 1:length(df.temps) 
+        time = round(typeof(1u"s"), (df.seconds[i] - df.seconds[1])u"s"
+                    + (df.nanosecs[i] - df.nanosecs[1])u"ns"|> u"s")
+        Plots.heatmap(df.heatmap[i], clims = (25, tmax+5), title = "$nm Thermocouple Heatmap",
+                xlabel = "Time: $(time)", yticks = collect(1:4), dpi = 200);
+    end
 
-PlotlyJS.plot(PlotlyJS.volume(x=X[:], y=Y[:], z=Z[:], value=values[:], opacity=0.7, surface_count = 500))
-
-    # anim = @animate for i in 1:length(df.temps) 
-        # time = round(typeof(1u"s"), (df.seconds[i] - df.seconds[1])u"s"
-                    # + (df.nanosecs[i] - df.nanosecs[1])u"ns"|> u"s")
-        # heatmap(df.heatmap[i], clims = (25, tmax+5), title = "$nm Thermocouple Heatmap",
-                # xlabel = "Time: $(time)", yticks = collect(1:4), dpi = 200)
+# Exporting the animation as an mp4 file
+mp4(anim, "./thermo_vids/hm_$nm.mp4");
 end
 
-# mp4(anim, "./thermo_vids/hm_$nm.mp4");
-
-
-
-# X, Y, Z = mgrid(1:4, 1:3, ustrip.(df.time))
-
-# values = sin.(pi .* X) .* cos.(pi .* Z) .* sin.(pi .* Y)
-
-
-# PlotlyJS.plot(PlotlyJS.volume(
-
-    # x=X[:],
-
-    # y=Y[:],
-
-    # z=Z[:],
-
-    # value=values[:],
-
-    # isomin=-0.1,
-    # isomax=0.8,
-    # opacity=0.1, # needs to be small to see through all surfaces
-    # surface_count=21, # needs to be a large number for good volume rendering
-
-# ))
+# I think this is working but It's refusing to run on my mac. I'll try it on a linux machine when I get home.
